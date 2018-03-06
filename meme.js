3a089038d4a47584aaee715095bdbfec2bef4b51
katycarr <khcarr9@gmail.com>
2018-03-06 11:54:55 -0500

    render comments

--- meme.js
+++ meme.js
@@ -7,7 +7,6 @@ class Meme {
     this.rating = rating;
     this.created_at = created_at;
     this.comments = comments;
-    this.createComments()
   }
 
   render() {
@@ -29,6 +28,7 @@ class Meme {
     if (this.text) {
       memeString += `<div class="description">${this.text}</div>`
     }
+    let comments = this.createComments()
     memeString += `</div>` //closing content div
     memeString += `<div class="extra content">
         <a>
@@ -37,16 +37,19 @@ class Meme {
         </a>
       </div>
       <a href="#" class="see-comments" data-id="${this.id}">See Comments</a>
-      <div class="comment-container" style="visibility:hidden"></div>`
+      <div class="comment-container" style="display:none">${comments}</div>`
     return memeString
   }
 
   createComments() {
+    let commentString = ''
     this.comments.forEach(commentData => {
       let comment = new Comment(commentData)
-      console.log(comment.render())
+      commentString += comment.render()
     })
+    return commentString
   }
+
 }
 
 // <div class="ui card">
